074568b4c5f42742c3950cdd9758c0415cb02238
dimountain <gftght@rambler.ru>
2022-12-28 15:40:17 +0300

    Выполнил задачу 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -14,7 +15,14 @@ public class Main {
     }
 
     public static void task2() {
+        System.out.println("Задача 2");
         String fullName = "Ivanov Ivan Ivanovich";
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
